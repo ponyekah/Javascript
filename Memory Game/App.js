0a0f7c3ad74d36b95062ea62eb5c490c691f436f
Princewill Onyekah <Onyekahprincewill@gmail.com>
2023-01-11 07:54:56 +0000

    update

---
+++ Memory Game/App.js
@@ -0,0 +1,51 @@
+const cardArray = [
+    {
+        name:  'fries',
+        img: 'https://cdn.britannica.com/34/206334-050-7637EB66/French-fries.jpg'
+    },
+    {
+        name:  'cheese',
+        img: 'https://cdn.britannica.com/60/217660-050-DBCC409A/cheddar-cheese-wedge.jpg'
+    },
+    {
+        name:  'hotdog',
+        img: 'https://www.seriouseats.com/thmb/82tLfYkrbqBkfRbLo7Yecr5neIs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__2014__05__20140508-292404-how-to-grill-hot-dogs-9af374204dd840ecb18a76692c5d423d.jpg'
+    },
+    {
+        name:  'ice-cream',
+        img: 'https://www.mydiasporakitchen.com/wp-content/uploads/2020/02/savingpng-3-3.png'
+    },
+    {
+        name:  'milkshake',
+        img: 'https://www.yourhomebasedmom.com/wp-content/uploads/2021/08/CHOCOLATE-MILK-SHAKE-5520.jpg'
+    },
+    {
+        name:  'pizza',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/800px-Eq_it-na_pizza-margherita_sep2005_sml.jpg'
+    },
+    {
+        name:  'fries',
+        img: 'https://cdn.britannica.com/34/206334-050-7637EB66/French-fries.jpg'
+    },
+    {
+        name:  'cheese',
+        img: 'https://cdn.britannica.com/60/217660-050-DBCC409A/cheddar-cheese-wedge.jpg'
+    },
+    {
+        name:  'hotdog',
+        img: 'https://www.seriouseats.com/thmb/82tLfYkrbqBkfRbLo7Yecr5neIs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__2014__05__20140508-292404-how-to-grill-hot-dogs-9af374204dd840ecb18a76692c5d423d.jpg'
+    },
+    {
+        name:  'ice-cream',
+        img: 'https://www.mydiasporakitchen.com/wp-content/uploads/2020/02/savingpng-3-3.png'
+    },
+    {
+        name:  'milkshake',
+        img: 'https://www.yourhomebasedmom.com/wp-content/uploads/2021/08/CHOCOLATE-MILK-SHAKE-5520.jpg'
+    },
+    {
+        name:  'pizza',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/800px-Eq_it-na_pizza-margherita_sep2005_sml.jpg'
+    },
+
+]
